308304efe774552b9b855f3f8dd0baf805100d4f
ryu-hedgefy <89095516+ryu-hedgefy@users.noreply.github.com>
2025-09-20 15:03:26 -0400

    changing chatbot's name

--- src/app.py
+++ src/app.py
@@ -20,24 +20,62 @@ load_dotenv()
 #from gtts import gTTS
 language = 'en'
 gptlang = 'english'
-#session = {}
+
 session = []
-session.append([])
 current_session = []
-current_session.append(session[-1])
+
+chatbotname = 'Xago'
+chatbotrole = f"You are a helpful AI assistant named {chatbotname} with a psychology background. You are very social and great at making friends. You provide answers that are straight to the point and full sentences, but try to stay under 50 words and occasionally when necessary go up to 200 words."
+
+def getnewsession():
+    newsession = [{"role": "system", "content": chatbotrole, "parts": [chatbotrole]}]
+    client = OpenAI(
+        api_key=os.getenv("OPENAI_API_KEY")
+    )
+    try:
+        response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            store=True,
+            messages=newsession
+        )
+        mytext = response.choices[0].message.content
+        newsession.append({"role": "model", "content": mytext, "parts": [mytext]})
+    except Exception as e:
+        print(f"Exception occurred: {e}")
+        mytext = "Sorry, there was an error processing your request."
+    session.append(newsession)
+    return newsession
+
+def restoresession(n_chats_ago):
+    if(len(current_session[0])) > 2:
+        session.append(session.pop(-(n_chats_ago+1)))
+    else:
+        session[-1] = session.pop(-(n_chats_ago+1))
+    return session[-1]
+
+def gethistory():
+    history = ""
+    for i in range(2,len(current_session[0])):
+        history = f"""
+                    {history}
+                    {current_session[0][i]['role']}: {current_session[0][i]['parts'][0]}
+                """
+    return history
+
+current_session.append(getnewsession())
+print(current_session)
 
 app = Flask(__name__)
 
 @app.route("/", methods=("GET", "POST"))
-def index():
+def index():    
     if request.method == "POST":
 
         model = request.form["model"]
 
         if model == "clear":
-            if len(session[-1]) > 0:
-                session.append([])
-            current_session[0] = session[-1]
+            if len(session[-1]) > 2:
+                current_session[0] = getnewsession()
             print(f"there are {len(session)} sessions")
             return render_template("index.html", result=None)
 
@@ -45,43 +83,30 @@ def index():
             chatnum = int(model.replace('previous',''))
             print(f"{len(session)} sessions and trying to go back {chatnum} chats")
             if len(session) > chatnum:
-                if(len(current_session[0])) == 0:
-                    session[-1] = session.pop(-(chatnum+1))
-                else:
-                    session.append(session.pop(-(chatnum+1)))
-                current_session[0] = session[-1]
+                current_session[0] = restoresession(chatnum)
                 mytext = f"restoring to {chatnum} chats ago"
-                mytext = markdown(mytext)
-                print(f"loading {chatnum} chats ago")
             else:
                 mytext = f"can't go back {chatnum} chats ago"
-            history = ""
-            for i in range(len(current_session[0])):
-                history = f"""
-                            {history}
-                            {current_session[0][i]['role']}: {current_session[0][i]['parts'][0]}
-                        """
-            history = markdown(history)
-            return render_template("index.html", result=mytext, history=history)
+            return render_template("index.html", result=markdown(mytext), history=markdown(gethistory()))
 
         else:
             prompt = request.form["prompt"]
-            print(prompt)
             current_session[0].append({"role": "user", "content": prompt, "parts": [prompt]})
-
+            print(current_session)
+            
             if model == "claude":
                 client = Anthropic(
                     api_key=os.getenv("ANTHROPIC_API_KEY")
                 )
                 try:
-                    messages = [{"role": msg["role"].replace("model","assistant"), "content": msg["content"]} for msg in current_session[0]]
+                    messages = [{"role": msg["role"].replace("model","assistant"), "content": msg["content"]} for msg in current_session[0] if msg["role"] != "system"]
                     response = client.messages.create(
                         model="claude-3-5-haiku-latest",
                         max_tokens=1000,
-                        messages = messages
+                        messages = messages,
+                        system = chatbotrole
                     )
                     mytext = response.content[0].text
-                    current_session[0].append({"role": "model", "content": mytext, "parts": [mytext]})
                 except Exception as e:
                     print(f"Exception occurred: {e}")
                     mytext = "Sorry, there was an error processing your request."
@@ -92,9 +117,8 @@ def index():
                 )
                 m = genai.GenerativeModel('gemini-2.5-flash')
                 try:
-                    response = m.generate_content([{"role": msg["role"], "parts": msg["parts"]} for msg in current_session[0]])
+                    response = m.generate_content([{"role": msg["role"], "parts": msg["parts"]} for msg in current_session[0] if msg["role"] != "system"])
                     mytext = response.text
-                    current_session[0].append({"role": "model", "content": mytext, "parts": [mytext]})
                 except Exception as e:
                     print(f"Exception occurred: {e}")
                     mytext = "Sorry, there was an error processing your request."
@@ -112,7 +136,6 @@ def index():
                         temperature=0.2,  # lower temperature for more deterministic answers
                     )
                     mytext = response.choices[0].message.content
-                    current_session[0].append({"role": "model", "content": mytext, "parts": [mytext]})
                 except Exception as e:
                     print(f"Exception occurred: {e}")
                     mytext = "Sorry, there was an error processing your request."
@@ -125,29 +148,21 @@ def index():
                     response = client.chat.completions.create(
                         model="gpt-4o-mini",
                         store=True,
-                        messages = [{"role": msg["role"].replace("model","assistant"), "content": msg["content"]} for msg in current_session[0]]
+                        messages = [{"role": msg["role"].replace("model","assistant"), "content": msg["content"]} for msg in current_session[0]],
                     )
                     mytext = response.choices[0].message.content
-                    current_session[0].append({"role": "model", "content": mytext, "parts": [mytext]})
                 except Exception as e:
                     print(f"Exception occurred: {e}")
                     mytext = "Sorry, there was an error processing your request."
             
+            current_session[0].append({"role": "model", "content": mytext, "parts": [mytext]})
             print(mytext)
-            history = ""
-            for i in range(len(current_session[0])):
-                history = f"""
-                            {history}
-                            {current_session[0][i]['role']}: {current_session[0][i]['parts'][0]}
-                        """
-            mytext = markdown(mytext)
-            history = markdown(history)
 
-            return render_template("index.html", result=mytext, prompt=prompt, history=history)
+            return render_template("index.html", result=markdown(mytext), prompt=prompt, history=markdown(gethistory()))
     else:
         return render_template("index.html", result=None)
 
 if __name__ == '__main__':
     signal(SIGINT, handler)
-    server_port = os.environ.get('PORT', '80')
+    server_port = os.environ.get('PORT', '5000')
     app.run(debug=False, port=server_port, host='0.0.0.0')
